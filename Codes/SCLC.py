b96453ed3163c1d9d42c12bc7bbd4f7560f5117e
mluginieski <72230152+mluginieski@users.noreply.github.com>
2020-10-16 15:31:29 -0300

    V.20201016

--- Codes/SCLC.py
+++ Codes/SCLC.py
@@ -1,7 +1,4 @@
-## Program dedicated to SCLC simulations
 import math
-
-# P3HT Eg = 1.15 eV DOI:10.1021/nl0707095
 
 e      = 1.6e-19  # [C]
 epson0 = 8.85e-12 # [F/m]
@@ -15,13 +12,12 @@ Nt     = 5e20     # [m^-3] DOI:10.1103/PhysRevB.81.085305
 n0     = 2.3e17   # [m^-3]
 nf     = 1e21     # [m^-3] DOI:10.1103/PhysRevB.81.085305
 Ef     = 5.53*e   # [J] Au Ref.: Ashcroft, N. W. and Mermin, N. D.
-# P3HT HOMO = 5.03 eV; LUMO = 7.03 eV DOI:10.1126/sciadv.1701508
 
 Et     = [5.535*e, 5.540*e, 5.545*e, 7.03*e] # [J]
 dV     = 1e-1
 
 
-fd = open("dados_SCLC.dat","w")
+fo = open("dados_SCLC.dat","w")
 
 Vc = (8/9) * (e * n0 * L**2)/(epson*epson0)
 print("%e " % Vc)
@@ -29,7 +25,7 @@ print("%e " % Vc)
 # Ohmic region
 while V <= Vc:
     J = e * n0 * mu * V/L
-    fd.write("%e %e \n" % (V,J))
+    fo.write("%e %e \n" % (V,J))
     V += dV
    
 # trap-SCLC region
@@ -44,12 +40,12 @@ for i in range(4):
     if theta == 1.0:
         while V <= 310.0:
             J = 1.125 * epson * epson0 * mu *((V**2)/(L**3))
-            fd.write("%e %e \n" % (V,J))
+            fo.write("%e %e \n" % (V,J))
             V += dV
             
     # single trap level 
     else:
         while V < Vtfl:
             J = 1.125 * epson * epson0 * mu * theta *((V**2)/(L**3))
-            fd.write("%e %e \n" % (V,J))
+            fo.write("%e %e \n" % (V,J))
             V += dV
